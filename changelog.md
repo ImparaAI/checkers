8d1f7b1d3257db87a0a4d788680663d270d3b362
Jan Hartigan <janhartigan13@gmail.com>
2018-09-14 15:01:34 -0700

    updating version numbers and adding changelog

---
+++ changelog.md
@@ -0,0 +1,7 @@
+## Changelog
+
+### 1.1.0
+- Autodeploying travis tag builds to pypi
+
+### 1.0.0
+- Initial release
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="imparaai-checkers",
-    version="1.0.0",
+    version="1.1.0",
     license='MIT',
     author="ImparaAI",
     author_email="author@example.com",
