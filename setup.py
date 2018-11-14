1958d0c8005089fd89e0469ad8d01cf3fabbab18
Jan Hartigan <janhartigan13@gmail.com>
2018-11-13 16:40:59 -0800

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name = "imparaai-checkers",
-    version = "1.3.0",
+    version = "1.4.0",
     license = 'MIT',
     author = "ImparaAI",
     author_email = "author@example.com",
